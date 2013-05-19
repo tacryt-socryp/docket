3f819a3193a61f0fa7ecb67eec435de4fb18c9bb
Logan Allen <loganfynne@gmail.com>
2013-05-19 13:55:24 -0400

    Increase size of container

--- app/view/MyContainer1.js
+++ app/view/MyContainer1.js
@@ -63,7 +63,7 @@ Ext.define('Booking.view.MyContainer1', {
 
                                         request.execute(function(resp) {
                                             console.log(resp);
-                                            w = 200 * resp.items.length;
+                                            w = 215 * resp.items.length;
                                             me.setSize(w,h);
                                             surface.setSize(w,h);
 
