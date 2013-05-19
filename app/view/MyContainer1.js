080e277145cbef5ff53ea6522d5fe09ee06c6d21
Logan Allen <loganfynne@gmail.com>
2013-05-19 13:57:46 -0400

    Adjusting a bit more

--- app/view/MyContainer1.js
+++ app/view/MyContainer1.js
@@ -63,7 +63,7 @@ Ext.define('Booking.view.MyContainer1', {
 
                                         request.execute(function(resp) {
                                             console.log(resp);
-                                            w = 210 * resp.items.length;
+                                            w = 205 * resp.items.length;
                                             me.setSize(w,h);
                                             surface.setSize(w,h);
 
