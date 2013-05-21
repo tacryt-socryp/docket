8d6c46192d423dc2f01e7c31698864792edb9a01
Logan Allen <loganfynne@gmail.com>
2013-05-21 19:16:44 -0400

    Displaced boxes to the left by 5 pixels

--- app/view/MyContainer1.js
+++ app/view/MyContainer1.js
@@ -107,7 +107,7 @@ Ext.define('Booking.view.MyContainer1', {
 
                                                 surface.add({
                                                     type: 'circle',
-                                                    cx: xloc+200,
+                                                    cx: xloc+195,
                                                     cy: 338,
                                                     r: 17,
                                                     fillStyle: boxColor
