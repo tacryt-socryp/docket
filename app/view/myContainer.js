bf1254bad28021bd221c2a2aa51641a445869707
Logan Allen <loganfynne@gmail.com>
2013-05-30 15:11:03 -0400

    Moved it down

--- app/view/myContainer.js
+++ app/view/myContainer.js
@@ -234,7 +234,7 @@ Ext.define('Conflux.view.myContainer', {
                                         font: '22px Arial',
                                         fill: '#FFF',
                                         x: xloc+45,
-                                        y: yloc+450
+                                        y: yloc+435
                                     }).show(true);
 
                                     if (description !== false) {
@@ -246,7 +246,7 @@ Ext.define('Conflux.view.myContainer', {
                                             height: 100,
                                             fill: '#FFF',
                                             x: xloc+45,
-                                            y: yloc+450
+                                            y: yloc+465
                                         }).show(true);
                                     }
 
