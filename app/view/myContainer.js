7aba80f17f87fb620e2d8b923fa48a9917b43ecd
Logan Allen <loganfynne@gmail.com>
2013-05-30 15:12:23 -0400

    Moved all dates/times to the left

--- app/view/myContainer.js
+++ app/view/myContainer.js
@@ -202,7 +202,7 @@ Ext.define('Conflux.view.myContainer', {
                                         text: dateTime,
                                         font: '14px Arial',
                                         fill: '#FFF',
-                                        x: xloc+170,
+                                        x: xloc+167,
                                         y: yloc+380
                                     }).show(true);
 
@@ -256,7 +256,7 @@ Ext.define('Conflux.view.myContainer', {
                                         text: dateTime,
                                         font: '14px Arial',
                                         fill: '#FFF',
-                                        x: xloc+170,
+                                        x: xloc+167,
                                         y: yloc+308
                                     }).show(true);
                                 }
