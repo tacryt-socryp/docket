1564736b18948a924b4b630c62bef7108e1c6fb2
Logan Allen <loganfynne@gmail.com>
2013-06-02 17:39:52 -0400

    Getting rid of fireAction

--- app/view/myContainer.js
+++ app/view/myContainer.js
@@ -341,7 +341,7 @@ Ext.define('Conflux.view.myContainer', {
                         child = me.query('#inlineDraw')[0];
                         child.events = resp.items;
                         console.log(child);
-                        child.fireAction('painted');
+                        child.fireEvent('painted');
                     });
                 });
             } else {
