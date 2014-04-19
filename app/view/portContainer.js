10dc630f9331f8106e6d33f10321cf8e58171ff5
Logan Allen <loganallc@gmail.com>
2014-04-19 15:20:29 -0400

    Fixing renames

--- app/view/portContainer.js
+++ app/view/portContainer.js
@@ -206,8 +206,8 @@ for (var a = 0; a < divider; a++) {
     for (var b = (description.length/divider)*(a+1); b > 0; b--) {
         
         if (description.substring(b, b+1) == ' ') {
-            console.log(measure.measureTextSingleLine(description.substring(0, b),"16px Arial").width - (a*xloc*9.8));
-            if (measure.measureTextSingleLine(description.substring(0, b),"16px Arial").width - 
+            console.log(m.measureTextSingleLine(description.substring(0, b),"16px Arial").width - (a*xloc*9.8));
+            if (m.measureTextSingleLine(description.substring(0, b),"16px Arial").width - 
                         (a*xloc*9.8) < (xloc*9.6)) {
                 description = description.substring(0,b) + '\n' + description.substring(b+1);
                 b = 0;
