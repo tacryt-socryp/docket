22d7f7bd1f2615a77f0aca7532c8f7b7dfe428f0
Logan Allen <loganallc@gmail.com>
2014-04-19 15:33:09 -0400

    parseInt

--- app/view/portContainer.js
+++ app/view/portContainer.js
@@ -205,7 +205,7 @@ function processDescription(description) {
 
 for (var a = 0; a < divider; a++) {
     
-    for (var b = (description.length/divider)*(a+1); b > 0; b--) {
+    for (var b = parseInt((description.length/divider)*(a+1)); b > 0; b--) {
         if (description.substring(b, b+1) == ' ') {
             console.log(b);
             console.log(m.measureTextSingleLine(description.substring(0, b),"16px Arial").width - (a*xloc*9.8));
