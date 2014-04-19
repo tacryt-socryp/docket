dbf440635b3a567578eda1b4e321ac88eafecc8d
Logan Allen <loganallc@gmail.com>
2014-04-19 16:35:33 -0400

    Making it work in all cases

--- app/view/portContainer.js
+++ app/view/portContainer.js
@@ -207,7 +207,23 @@ function processDescription(description) {
 for (var a = 0; a < divider; a++) {
     
     for (var b = parseInt((description.length/divider)*(a+1)); b > 0; b--) {
-        if (description.substring(b, b+1) == ' ') {
+        if (divider > 1) {
+            if (description.substring(b, b+1) == ' ') {
+            console.log(b);
+            console.log(m.measureTextSingleLine(description.substring(0, b),"16px Arial").width 
+                        (xloc*9.8) - sum);
+            if (m.measureTextSingleLine(description.substring(0, b),"16px Arial").width 
+                        (xloc*9.8) - sum < (xloc*9.8)) {
+                sum = m.measureTextSingleLine(description.substring(0, b),"16px Arial").width - (xloc*9.8);
+                console.log("picked this value");
+                
+                description = description.substring(0,b) + '\n' + description.substring(b+1);
+                b = 0;
+                vDisplaceDesc = vDisplaceDesc+5;
+            }
+            }
+        } else {
+            if (description.substring(b, b+1) == ' ') {
             console.log(b);
             console.log(m.measureTextSingleLine(description.substring(0, b),"16px Arial").width 
                         - sum);
@@ -219,6 +235,7 @@ for (var a = 0; a < divider; a++) {
                 description = description.substring(0,b) + '\n' + description.substring(b+1);
                 b = 0;
                 vDisplaceDesc = vDisplaceDesc+5;
+            }
             }
         }
     }
