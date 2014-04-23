0ffa48fe89e9e1aeb7a2ffcc82bd266493897a91
Logan Allen <loganallc@gmail.com>
2014-04-23 18:19:56 -0400

    Remove all sprites from surface then redraw

--- app/view/portContainer.js
+++ app/view/portContainer.js
@@ -472,6 +472,7 @@ gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, funct
             console.log(resp);
             if (Ext.isDefined(resp) && Ext.isDefined(resp.items) && Ext.isDefined(resp.items[0])) {
                 child.events = resp.items;
+                child.surface.removeAll();
                 child.element.redraw();
             } else {
                 setTimeout(function(){
