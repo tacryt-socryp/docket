dda7d13a2555b4fb960bb098576702b40b5bef73
Logan Allen <loganallc@gmail.com>
2014-04-24 14:53:16 -0400

    Moved name of room to the right

--- app/view/portContainer.js
+++ app/view/portContainer.js
@@ -69,7 +69,7 @@ fn: function(element, eOpts) {
     //addRect(timelineColor, w, 50, 0, 0, 0);
 
     //Name of room
-    addText("#fff", "20px Arial", roomText, 30, 30);
+    addText("#fff", "20px Arial", roomText, 35, 30);
     //addRect(boxColor, 40, 40, w-55, 7, 3);
     addText("#fff", "24px Arial", "+", w-42, 30);
     
